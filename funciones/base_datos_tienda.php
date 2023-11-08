65a59735ea73e4cde3d04afa3df5dd01c3b11a95
IlloJuanma <JRM1392@hotmail.com>
2023-11-08 08:50:30 +0100

    Prueba

--- funciones/base_datos_tienda.php
+++ funciones/base_datos_tienda.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
     $_servidor ='localhost'; // Esto es para almacenar la ip
-=======
-    /*$_servidor ='localhost'; // Esto es para almacenar la ip
->>>>>>> 6335d655867b95f2ec0c06fbe0d6eebb3033f116
     $_usuario = 'root';
     $_contrasena = 'medac';
     $_base_de_datos = 'db_tienda';
@@ -13,5 +9,4 @@
         or die("Error de conexión");
 =======
         or die("Error de conexión");*/
->>>>>>> 6335d655867b95f2ec0c06fbe0d6eebb3033f116
 ?>
